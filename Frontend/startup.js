6722e88b8bf6787f3c8a2fbfb2e2ba1c1c6b27c4
renathaux <charlesdieufort2@gmail.com>
2026-08-17 15:36:40 -0400

    Bump SMC settings asset after renderer wiring fix

--- Frontend/startup.js
+++ Frontend/startup.js
@@ -4,7 +4,7 @@
       document.write('<script src="chart/live-candles/live-candle-controller.js?v=3"><\/script>');
     }
     document.write('<link rel="stylesheet" href="indicators/smc/smc.css?v=4">');
-    document.write('<script src="indicators/smc/smc-settings.js?v=6"><\/script>');
+    document.write('<script src="indicators/smc/smc-settings.js?v=7"><\/script>');
     document.write('<script src="indicators/smc/smc-renderer.js?v=9"><\/script>');
     document.write('<script src="indicators/smc/smc-indicator.js?v=4"><\/script>');
     document.write('<script src="indicators/smc/smc-chart-bridge.js?v=6"><\/script>');
