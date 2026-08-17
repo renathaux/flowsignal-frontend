671c8871f228de6ef2dccae7a63d6f26c22f2934
renathaux <charlesdieufort2@gmail.com>
2026-08-17 14:37:55 -0400

    Bump local visual SMC asset version

--- Frontend/apiClient.js
+++ Frontend/apiClient.js
@@ -97,5 +97,5 @@
   loadScriptOnce('script[data-flow-smc-renderer-port]', "indicators/smc/smc-renderer.js?v=6", "flowSmcRendererPort", "FLOW_SMC_RENDERER_PORT_LOAD_FAILED");
   loadScriptOnce('script[data-flow-smc-chart-bridge]', "indicators/smc/smc-chart-bridge.js?v=5", "flowSmcChartBridge", "FLOW_SMC_CHART_BRIDGE_LOAD_FAILED");
   loadScriptOnce('script[data-flow-smc-local-engine]', "indicators/smc/smc-local-engine.js?v=3", "flowSmcLocalEngine", "FLOW_SMC_LOCAL_ENGINE_LOAD_FAILED");
-  loadScriptOnce('script[data-flow-smc-local-visual]', "indicators/smc/smc-local-visual.js?v=3", "flowSmcLocalVisual", "FLOW_SMC_LOCAL_VISUAL_LOAD_FAILED");
+  loadScriptOnce('script[data-flow-smc-local-visual]', "indicators/smc/smc-local-visual.js?v=4", "flowSmcLocalVisual", "FLOW_SMC_LOCAL_VISUAL_LOAD_FAILED");
 })();
