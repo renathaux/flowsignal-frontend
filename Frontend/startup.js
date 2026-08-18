557c9ed942feca48f13f6fdfd24b0e1ab7fcc473
renathaux <charlesdieufort2@gmail.com>
2026-08-18 00:48:17 -0400

    Load isolated Forex signal state display module

--- Frontend/startup.js
+++ Frontend/startup.js
@@ -42,6 +42,7 @@
     document.write('<script src="indicators/smc/smc-chart-bridge.js?v=6"><\/script>');
     document.write('<script src="indicators/smc/smc-local-engine.js?v=4"><\/script>');
     document.write('<script src="indicators/smc/smc-local-visual.js?v=6"><\/script>');
+    document.write('<script src="signal-display-state.js?v=1"><\/script>');
   }
 
   function loadTabRoleSession() {
